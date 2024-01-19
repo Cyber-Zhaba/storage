15744de77a591cbc477f6011c49bcf5d2602c880
Fedor Lebedev <95484719+DrPepper1337@users.noreply.github.com>
2024-01-20 00:57:13 +0300

    fixed registration and added some backend features

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'prev_prof_lovers_secret_key'
 
 
+@app.errorhandler(404)
+def not_found(error):
+    print(error)
+    return render_template('404.html')
+
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -50,6 +56,8 @@ def sign_up():
             session = db_session.create_session()
             if session.query(User).filter(User.login == f'{form.login.data}').first():
                 message = "Этот логин уже существует"
+            elif session.query(User).filter(User.email == f'{form.email.data}').first():
+                message = "Этот email уже привязан к акаунту"
             else:
                 post('http://localhost:5000/api/users', json={
                     'login': form.login.data,
@@ -58,13 +66,43 @@ def sign_up():
                      timeout=(2, 20))
                 user = session.query(User).filter(User.login == f'{form.login.data}').first()
                 login_user(user, remember=form.remember_me.data)
+                return redirect("/", 301)
             session.close()
-            return redirect('/', 302)
     return render_template('sign_up.html', title='Регистрация', form=form, message=message)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message, form = '', LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session = db_session.create_session()
+            user = session.query(User).filter(User.login == form.login.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/", 301)
+            message = "Неправильный логин или пароль"
+            session.close()
+    return render_template('login.html', title='Логин', form=form, message=message)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return render_template('main.html', title='Главная страница')
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('profile.html', title='Главная страница')
+
+
+@app.route('/user_profile', methods=['GET', 'POST'])
+def user_profile():
+    return render_template('user_profile.html', title='Главная страница')
 
 
 if __name__ == '__main__':
     api.add_resource(UserListResource, '/api/users')
+    api.add_resource(UserResource, '/api/<int:user_id>')
     db_session.global_init("data/data.db")
-    http = WSGIServer(('0.0.0.0', 5000), app.wsgi_app)
-    http.serve_forever()
+    app.run(debug=True, host='0.0.0.0')
